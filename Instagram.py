2f2030ec55c3de264a84e4d0cc39e0cff5237b84
Dhruv Athaide <athaidedhruv@gmail.com>
2023-11-05 00:51:30 +0530

    first commit

---
+++ Instagram.py
@@ -0,0 +1,180 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+import os 
+import re
+from bs4 import BeautifulSoup
+import csv
+
+# Instagram Login Credentials
+username = "Enter Your Username/Email"
+password = "Enter Your Password"
+
+# XLSX file to read all user profiles provided
+data = pd.read_excel("profile_links.xlsx", header=None, names=['Profile Links'])
+
+profile_links = data['Profile Links'].tolist()
+
+# Configuring the Chrome driver and handling Notification Alert
+options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications": 2}
+options.add_experimental_option("prefs", prefs)
+
+# Fullscreen mode
+options.add_argument("--start-maximized")
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+# Opening the Instagram Website
+driver.get("https://www.instagram.com/")
+
+# Finding and Inputting the Username
+username_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, 'username'))
+)
+username_field.send_keys(username)
+
+# Finding and Inputting the Password
+password_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, 'password'))
+)
+password_field.send_keys(password)
+
+# Finding and Clicking the Login Button
+login_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
+)
+login_button.click()
+
+# Time Delay to allow Login Process
+time.sleep(7)
+
+# Create a folder to store the text files
+textoutput_folder = "UnrefinedData"
+os.makedirs(textoutput_folder, exist_ok=True)
+
+# Create a folder to store the CSV files
+csvoutput_folder = "RefinedData"
+os.makedirs(csvoutput_folder, exist_ok=True)
+
+
+# Iterate through each profile link
+for profile_link in profile_links:
+    try:
+        # Extract the profile name from the profile link
+        profile_name = profile_link.split("/")[-2]  # Assuming the profile link format is "https://www.instagram.com/{profile_name}/"
+
+        # Adjust the profile link to include "followers" to go to the followers page of the profile
+        profile_link_followers = profile_link + "followers"
+
+        # Go to the user's profile
+        driver.get(profile_link_followers)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//body")))  # Wait for the page to load
+
+        # Get the body element
+        body_element = driver.find_element(By.XPATH, "//body")
+
+        # Wait for some time to ensure the page is fully loaded before scrolling
+        time.sleep(5)
+
+        # Xpath for the Followers Page to have it focused
+        target_element_xpath = "/html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]"
+        target_element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, target_element_xpath))
+        )
+
+        # Use ActionChains to put the focus on the followers page so that it can scroll
+        actions = ActionChains(driver)
+        actions.move_to_element(target_element).click().perform()
+
+        # Scroll the main profile page for 20 seconds
+        duration_seconds = 20  # 20 seconds
+        scroll_interval = 1  # Scroll every 1 second (1 scroll per second)
+        total_scrolls = int(duration_seconds / scroll_interval)  # Ensure total_scrolls is an integer
+        for _ in range(total_scrolls):
+            try:
+                # Scroll the main profile page
+                body_element.send_keys(Keys.PAGE_DOWN)
+
+                time.sleep(scroll_interval)
+            except Exception as e:
+                print("Error scrolling:", e)
+
+        # Data Extraction for the Instagram Followers
+        
+        # Find the target element by XPath
+        target_element_xpath = "//div[@role='dialog']"
+        target_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, target_element_xpath))
+        )
+
+        # Get the inner text of the element using JavaScript
+        element_text = driver.execute_script("return arguments[0].outerHTML;", target_element)
+
+        # Save the text to a text file with a unique name based on the profile inside the output folder
+        textoutput_file_name = os.path.join(textoutput_folder, f"{profile_name}_output.txt")
+        with open(textoutput_file_name, "w", encoding="utf-8") as file:
+            file.write(element_text)
+
+        #Refining the Extracted Data
+        def extract_usernames(input_file):
+            with open(input_file, 'r', encoding='utf-8') as input_f:
+                # Extract usernames using regex
+                usernames = []
+                for line in input_f:
+                    span_matches = re.findall(r'dir="auto">([^<]+)</span>', line)
+                    usernames.extend(span_matches)
+
+            return usernames
+
+        def extract_names_from_html(html_content):
+            # Parse HTML content
+            soup = BeautifulSoup(html_content, 'html.parser')
+
+            # Extract names in the specified format
+            names = [span.get_text(strip=True) for span in soup.find_all('span', class_='x1lliihq x193iq5w x6ikm8r x10wlt62 xlyipyv xuxw1ft')]
+            return names
+
+        if __name__ == "__main__":
+            # Save the text to a text file with a unique name based on the profile inside the output folder
+            csvoutput_file_name = os.path.join(csvoutput_folder, f"{profile_name}_output.csv")
+            
+            # Paths to input and output files
+            input_file = textoutput_file_name
+            output_file = csvoutput_file_name
+
+            # Extract usernames from Text File
+            instagram_usernames = extract_usernames(input_file)
+
+            # Read HTML content from Text File
+            with open(input_file, 'r', encoding='utf-8') as file:
+                html_content = file.read()
+
+            # Extract names from Text File
+            instagram_names = extract_names_from_html(html_content)
+
+            # Combine results and write to CSV File
+            with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+                fieldnames = ['Full Name', 'Username']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                writer.writeheader()
+                for name_code2, username_code1 in zip(instagram_names, instagram_usernames):
+                    writer.writerow({'Full Name': name_code2, 'Username': username_code1})
+
+            print(f"Results saved to '{output_file}'")
+
+        # Navigate back to the user's profile
+        driver.back()
+
+    except Exception as e:
+        print("Error processing profile:", profile_link, ":", e)
+
+# Close the browser window
+driver.quit()
---
+++ README.md
@@ -0,0 +1,61 @@
+# Instagram Automated Followers Scraping Tool
+
+I have created a Instagram Automated Followers Scraping Tool using Python to scrape all the followers of the profile links you provide using selenium!
+
+
+## Installation
+
+To install and run this project,
+
+You can download the zip file or Clone the Project Repository using Git with the below command:
+```bash
+git clone https://github.com/DhruvAthaide/Instagram-Automated-Followers-Scraper.git
+```
+
+Once, you have installed the Repository then you can cd into the directory and pip install the requirements needed to run the tool:
+```bash
+cd Instagram-Automated-Followers-Scraper
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+Then, you need to create a XLSX File or Excel File and name it:
+```bash
+Name: profile_links.xlsx
+```
+
+Then, you need to set the following column name in the Excel File and paste the Instagram profile's link you want to message in this column:
+```bash
+Column 1: Profile Links
+```
+
+Then, in the 'Instagram.py' file on Line 16 & 17 Enter your Username/Email ID and Password in between the Quotes for the String:
+```bash
+username = "Enter Your Username/Email"
+password = "Enter Your Password"
+```
+
+If you want to change how many hours it will scrape each profile's followers list, then change the following line to suit the time you want (Scrolling for above 2 hours might get the account suspended):
+```bash
+duration_seconds = 20  # 20 seconds
+```
+
+If you want to change how many scrolls per second, then change the following line to suit your neeeds:
+```bash
+scroll_interval = 1  # Scroll every 1 second (1 scroll per second)
+```
+
+Then, you can simply run the python file and not touch anything and it will execute the message sending to the Instagram profile's provided in the XLSX or Excel File and all the profile link's followers will appear in a csv file in the RefinedData Folder and if you want the Unrefined Data it will be in the UnrefinedData Folder.
+
+## Authors
+
+- [@Dhruv Athaide](https://github.com/DhruvAthaide)
+
+
+## Languages & Tools Used:
+<p align="left"> 
+<a href="https://www.python.org/" target="_blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a>
+<a href="https://www.selenium.dev/" target="_blank" rel="noreferrer"> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg" alt="selenium" width="40" height="40"/> </a>
+</p>
